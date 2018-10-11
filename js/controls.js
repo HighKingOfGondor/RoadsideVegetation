cc6b3d165d16c1fcfbe7de893f754ee73ae109aa
Jared <jaredwheeler94@tamu.edu>
2018-10-11 14:36:12 -0500

    buttons work for navigation

--- js/controls.js
+++ js/controls.js
@@ -1,16 +1,19 @@
 $(document).ready(function () {
+
+   let page = 1;
+
    $("#slide-two-content").hide();
    $("#slide-three-content").hide();
    $("#final-slide").hide();
 
    $("#slide-1").on("click", function () {
-       $("#slide-one-content").show(1100);
+       $("#slide-one-content").show(500);
        $("#slide-two-content").hide();
        $("#slide-three-content").hide();
        $("#final-slide").hide();
    });
    $("#slide-2").on("click", function () {
-       $("#slide-two-content").show(1100);
+       $("#slide-two-content").show(500);
 
        $("#slide-one-content").hide();
        $("#slide-three-content").hide();
@@ -18,17 +21,86 @@ $(document).ready(function () {
    });
 
    $("#slide-3").on("click", function () {
-       $("#slide-three-content").show(1100);
+       $("#slide-three-content").show(500);
 
        $("#slide-one-content").hide();
        $("#slide-two-content").hide();
        $("#final-slide").hide();
    });
    $("#slide-4").on("click", function () {
-       $("#final-slide").show(1100);
+       $("#final-slide").show(500);
 
        $("#slide-one-content").hide();
        $("#slide-two-content").hide();
        $("#slide-three-content").hide();
    });
+
+   $("#prev-slide").on("click", function () {
+       if (page > 1) {
+
+           page -= 1;
+
+           switch (page) {
+               case 1: {
+                   $("#slide-one-content").show(500);
+                   $("#slide-two-content").hide();
+                   $("#slide-three-content").hide();
+                   $("#final-slide").hide(); break;
+               } case 2: {
+                   $("#slide-two-content").show(500);
+
+                   $("#slide-one-content").hide();
+                   $("#slide-three-content").hide();
+                   $("#final-slide").hide(); break;
+               } case 3: {
+                   $("#slide-three-content").show(500);
+
+                   $("#slide-one-content").hide();
+                   $("#slide-two-content").hide();
+                   $("#final-slide").hide(); break;
+               } case 4: {
+                   $("#final-slide").show(500);
+
+                   $("#slide-one-content").hide();
+                   $("#slide-two-content").hide();
+                   $("#slide-three-content").hide(); break;
+               }
+           }
+       }
+   });
+
+    $("#next-slide").on("click", function () {
+        if (page < 4) {
+
+            page += 1;
+
+            switch (page) {
+                case 1: {
+                    $("#slide-one-content").show(500);
+                    $("#slide-two-content").hide();
+                    $("#slide-three-content").hide();
+                    $("#final-slide").hide(); break;
+                } case 2: {
+                    $("#slide-two-content").show(500);
+
+                    $("#slide-one-content").hide();
+                    $("#slide-three-content").hide();
+                    $("#final-slide").hide(); break;
+                } case 3: {
+                    $("#slide-three-content").show(500);
+
+                    $("#slide-one-content").hide();
+                    $("#slide-two-content").hide();
+                    $("#final-slide").hide(); break;
+                } case 4: {
+                    $("#final-slide").show(500);
+
+                    $("#slide-one-content").hide();
+                    $("#slide-two-content").hide();
+                    $("#slide-three-content").hide(); break;
+                }
+            }
+        }
+    });
+
 });
